556340b028ad5fc133ca026da5db6a4644f8e622
Paul Estrada <paulestrada@Pauls-MBP.local.tld>
2019-08-09 15:40:48 -0500

    ip_config

--- cliente.py
+++ cliente.py
@@ -9,7 +9,7 @@ f.close()
 print(ip)
 
 parametros = {'USUARIO':'pdestrad@gmail.com', 'CONTRASENA': 'homoplato1'}
-
-r = requests.post('http://127.0.0.1:8000/rest/login', data=json.dumps(parametros))
+url = ip + '/rest/login'
+r = requests.post(url, data=json.dumps(parametros))
 respuesta = json.dumps(r.json())
 print(respuesta)
