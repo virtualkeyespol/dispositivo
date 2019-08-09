5810fd4372865ac26674984e285195fc2b04abc5
Paul Estrada <paulestrada@Pauls-MBP.local.tld>
2019-08-09 15:38:30 -0500

    ip_config

--- cliente.py
+++ cliente.py
@@ -1,6 +1,13 @@
 import requests
 import json
 
+##	LEER NUMERO DE SERIE DEL DISPOSITIVO
+f=open("ip_config.txt", "r")
+ip = str(f.read())
+f.close()
+
+print(ip)
+
 parametros = {'USUARIO':'pdestrad@gmail.com', 'CONTRASENA': 'homoplato1'}
 
 r = requests.post('http://127.0.0.1:8000/rest/login', data=json.dumps(parametros))
